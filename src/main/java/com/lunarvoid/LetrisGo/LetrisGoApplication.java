a039291fd2794bc238502934063f398d02ad0678
Pedro Maia <pedrodrm250@gmail.com>
2026-04-07 16:17:21 -0300

    commit inicial

---
+++ src/main/java/com/lunarvoid/LetrisGo/LetrisGoApplication.java
@@ -0,0 +1,13 @@
+package com.lunarvoid.LetrisGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetrisGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetrisGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunarvoid/LetrisGo/LetrisGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunarvoid.LetrisGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetrisGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
